a28698f37e57cd20533e8398c11efee65011bb4e
talalalamdar <talalalamdar@gmail.com>
2017-12-06 16:14:48 +0100

    ascas

--- week-2/week-2.js
+++ week-2/week-2.js
@@ -8,16 +8,16 @@ space();
 // Step 1
 
 let numbers = [1, 2, 3, 4];
-console.log("The original array is: " , numbers);       
+console.log("The original array is: ", numbers);
 let filteredNumbers = numbers.filter(num => num % 2 !== 0);  // filtering the odd numbers in the original array
-console.log("The filtered array is: ", filteredNumbers);     // returning the filtered array 
-let newArr = filteredNumbers.map(num => num * 2);            // doubling the filtered array 
-console.log("The doubled array is : " , newArr);             // returning the doubled array 
+console.log("The filtered array is: ", filteredNumbers);     // returning the filtered array
+let newArr = filteredNumbers.map(num => num * 2);            // doubling the filtered array
+console.log("The doubled array is : ", newArr);             // returning the doubled array
 
 space();
 // Step 2
 
-let final = numbers.reduce((x, z) => {
+let finalArray = numbers.reduce((x, z) => {
   if (z % 2 === 0) {
     x.push(z, z);              // pushing the even numbers twice to the empty array (final)
   } else {
@@ -25,8 +25,8 @@ let final = numbers.reduce((x, z) => {
   }
   return x
 }, []);
-console.log("The final array of  numbers are : " , final);  
- 
+console.log("The final array of  numbers are : ", finalArray);
+
 space();
 // Step 3.1
 
@@ -76,19 +76,19 @@ let tasks = [monday, tuesday];
 
 
 
-let totalDuration = 0;              // declaring a variable to calculate the total duration in minutes 
-let tasksList = [];                 // declaring an array to display a list of all tasks during the two days 
+let totalDuration = 0;              // declaring a variable to calculate the total duration in minutes
+let tasksList = [];                 // declaring an array to display a list of all tasks during the two days
 let calc = tasks.map(i => {
-  for (let j in i) {                // looping through tasks to calculate the durations
-    totalDuration += i[j].duration;        
-    tasksList.push(i[j].name);                // pushing the task name to the array 
+  for (let j in i) {                // looping through tasks to calculate the durations++
+    totalDuration += i[j].duration;
+    tasksList.push(i[j].name);                // pushing the task name to the array
   }
 });
 console.log("The total time that has been spend on tasks : " + totalDuration + " minutes.");
 console.log("list of all tasks during the two days : ");
 function list() {
-  for (let i = 0; i < tasksList.length; i++) {           // a for loop inside a function to display on the console a list of all tasks
-    console.log((i + 1) + " - " + tasksList[i] + ".")    // during the two days
+  for (let i = 0; i < tasksList.length; i++) {           // a for loop inside a function to display on the console a list of 
+    console.log((i + 1) + " - " + tasksList[i] + ".")    // all tasks during the two days
   }
 }
 list();
@@ -99,7 +99,7 @@ space();
 
 let convertedTasks = tasks.map(i => {                 // converting the durations of tasks from minutes to hours
   for (let j in i) {
-     i[j].duration /= 60;
+    i[j].duration /= 60;
   }
 })
 console.log(tasks);
@@ -117,11 +117,11 @@ let filteredHours = tasks.filter(i => {                // filtering the tasks th
 });
 
 space();
-// Step 3.4 
+// Step 3.4
 
 
-let totalConvertedDuration = tasks.reduce((a, i) => {       // summing all the durations on tasks 
-  for  (let j in i) {
+let totalConvertedDuration = tasks.reduce((a, i) => {       // summing all the durations on tasks
+  for (let j in i) {
     a += i[j].duration;
   }
   return Math.round(a);
@@ -134,22 +134,12 @@ space();
 // Step 3.5
 
 let paying = () => {                // a function to calculate how much she should gain
-   let  result = totalConvertedDuration * 12;
+  let result = totalConvertedDuration * 12;
   console.log("she should make 12.00€ per hour, and in total for two days " + result + "€ .")
-}  
+}
 paying();
 
 
 space();
-
-
-
-
-
-
-
-
-
-
 
 
